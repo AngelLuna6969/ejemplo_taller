5b6ce1e8f98c5e4c5dd1443b8499487a487cef47
Ángel Luna <utp0149900@alumno.utpuebla.edu.mx>
2023-04-24 10:22:08 -0600

    Creacion de controlador taller

---
+++ application/views/Taller/index_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vista por defecto</title>
+</head>
+<body>
+    <h1>Controlador taller</h1>
+    <h2>Vista por defecto</h2>
+</body>
+</html>
